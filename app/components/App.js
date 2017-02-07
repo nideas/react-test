45069e1e9f945b39adc8d7ca691817c66923fc84
Per Karlsson <per@pkarlsson.com>
2017-02-07 20:20:24 +0000

    first commit..

---
+++ app/components/App.js
@@ -0,0 +1,12 @@
+var React = require('react');
+
+
+var App = React.createClass({
+    render: function() {
+        return (
+            <div>Cloud9 React config and test before Heroku deploy.</div>
+        );
+    }
+});
+
+module.exports = App;
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var HTMLWebpackPlugin = require('html-webpack-plugin');
+var HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+    template: __dirname + '/app/index.html',
+    filename: 'index.html',
+    inject: 'body'
+});
+
+
+var port = process.env.PORT || 8080;
+var host = process.env.IP || '127.0.0.1';
+
+module.exports = {
+    entry: __dirname + '/app/index.js',
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
+        ]
+    },
+    output: {
+        filename: 'transformed.js',
+        path: __dirname + '/build'
+    },
+    plugins: [HTMLWebpackPluginConfig],
+      devServer: {
+    port: port,
+    host: host
+  }
+};
+
